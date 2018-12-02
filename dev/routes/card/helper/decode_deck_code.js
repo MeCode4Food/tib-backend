05d99d5c06df54704fff243318db0c44770709eb
CK L <cheekhuin.loong@gmail.com>
2018-12-03 00:04:29 +0800

    removed checkfor length and cardId - for draft decks

--- dev/routes/card/helper/decode_deck_code.js
+++ dev/routes/card/helper/decode_deck_code.js
@@ -6,7 +6,6 @@ module.exports = async (deckCode) => {
   let deckObjectRaw = deckDecoder.ParseDeck(deckCode)
 
   let cardIdArray = []
-  let totalCardIds = deckObjectRaw.heroes.length + deckObjectRaw.cards.length
   let idToDelete = []
 
   // loop through heroes and cards to get card_id and put all of them into array cardIdArray
@@ -24,7 +23,6 @@ module.exports = async (deckCode) => {
     .whereIn(`card_id`, cardIdArray)
 
   let results = await deckCardsQuery
-  if (results.length !== totalCardIds) { throw new Error('Please check the the deck code') }
 
   // create a map to avoid searching through array
   let resultsMap = {}
