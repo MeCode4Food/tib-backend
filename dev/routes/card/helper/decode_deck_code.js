7ed4eac491a7946ecc74de5da9c7090132ad9e6e
CK L <cheekhuin.loong@gmail.com>
2018-12-04 08:04:49 +0800

    fixed deck decoder on duplicate hero cards for draft

--- dev/routes/card/helper/decode_deck_code.js
+++ dev/routes/card/helper/decode_deck_code.js
@@ -6,7 +6,7 @@ module.exports = async (deckCode) => {
   let deckObjectRaw = deckDecoder.ParseDeck(deckCode)
 
   let cardIdArray = []
-  let totalCardIds = deckObjectRaw.heroes.length + deckObjectRaw.cards.length
+  // let totalCardIds = deckObjectRaw.heroes.length + deckObjectRaw.cards.length
   let idToDelete = []
 
   // loop through heroes and cards to get card_id and put all of them into array cardIdArray
@@ -24,7 +24,7 @@ module.exports = async (deckCode) => {
     .whereIn(`card_id`, cardIdArray)
 
   let results = await deckCardsQuery
-  if (results.length !== totalCardIds) { throw new Error('Please check the the deck code') }
+  // if (results.length !== totalCardIds) { throw new Error('Please check the the deck code') }
 
   // create a map to avoid searching through array
   let resultsMap = {}
@@ -38,13 +38,16 @@ module.exports = async (deckCode) => {
     const id = stack.id
     const turn = stack.turn
 
-    stack = resultsMap[id]
+    Object.assign(stack, resultsMap[id])
     stack.turn = turn
 
+    console.log(stack.card_id, stack.turn)
     return stack
   })
 
   deckObjectRaw.heroes = _.sortBy(deckObjectRaw.heroes, (hero) => { return hero.turn })
+
+  console.log(deckObjectRaw.heroes)
 
   // do the same for cards
   deckObjectRaw.cards = _.map(deckObjectRaw.cards, (stack) => {
