1498c8f733dc83035b238f17c4330e1cf7a00364
CK L <cheekhuin.loong@gmail.com>
2018-12-23 17:42:06 +0800

    added hourlyActivity endpoint

---
+++ dev/routes/user/helpers/validate_hourly_activity.js
@@ -0,0 +1,19 @@
+const Joi = require('joi')
+const { PassphraseError } = require(`${global.SERVER_ROOT}/services/response/error`)
+const { API_SECRET } = process.env
+
+module.exports = (queryObject) => {
+  return new Promise((resolve, reject) => {
+    Joi.validate(queryObject, Joi.object().keys({
+      'date': Joi.string().isoDate(),
+      'hour': Joi.number().integer().positive().max(24),
+      'online': Joi.number().integer().positive(),
+      'in_game': Joi.number().integer().positive(),
+      'total': Joi.number().integer().positive(),
+      'secret': Joi.string().required().valid([API_SECRET]).error(new PassphraseError())
+    }), (error, value) => {
+      if (error) reject(error)
+      resolve(value)
+    })
+  })
+}
