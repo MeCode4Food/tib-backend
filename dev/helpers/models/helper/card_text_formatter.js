0bbb309ee310c7e2c96efa6e65cdc4a8c8ed730c
CK L <cheekhuin.loong@gmail.com>
2018-12-25 20:48:28 +0800

    changed the way how html tags are converted

--- dev/helpers/models/helper/card_text_formatter.js
+++ dev/helpers/models/helper/card_text_formatter.js
@@ -1,7 +1,7 @@
 exports.replaceHTMLwithMD = (text) => {
   //  replace opening and closing tags with markdown bold tags
   text = text.replace(/<span style='font-weight:bold.+?;'>/g, '**')
-    .replace(/<\/span>/g, '**')
+    .replace(/<\/span>/g, '** ')
     .replace(/<[bB][rR]\/*>/g, '') // all flavours of br
 
   return text
